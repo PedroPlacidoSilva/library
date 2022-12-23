2e183065b17d734f6831498c134240eebe4e0a3a
Pedro Placido Silva <ppspedro@gmail.com>
2022-12-23 19:55:10 +0000

    Concluded the display and insert books. Made initial styling of the books presentation

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let myLibrary = [];
+let myLibrary = [
+  ["The Odin List of Treasures by Odin", "220", "read"],
+  ["The Emperor by Shiklun", "145", "read"],
+  ["The Girl of the Blue Train by Shiklun", "173", "not read yet"],
+  ["The Shinoby of the Sand by Ninpo", "85", "not read yet"],
+];
 
 // Create a new Book with all characteristics
 class Book {
@@ -20,6 +25,40 @@ class Book {
   }
 }
 
+// Pedro
+function displayBooks(books) {
+  // Delete all elements inside the cards div element (In order to present the updated library)
+  const delBooks = document.getElementById("cards");
+  delBooks.innerHTML = "";
+
+  const cardsContainer = document.getElementById("cards");
+  for (let i = 0; i < books.length; i++) {
+    // Create card for the book and set id to the card+number of index of the array and a class card
+    const div = document.createElement("div");
+    div.id = "card" + i.toString();
+    div.classList.add("card");
+    // Create element and set the first element of each book on html
+    const titleBook = document.createElement("p");
+    titleBook.classList.add("card-content");
+    titleBook.textContent = `Book: ${books[i][0]}`;
+    // Create element and set the second element of each book on html
+    const pagesBook = document.createElement("p");
+    pagesBook.classList.add("card-content");
+    pagesBook.textContent = `Pages: ${books[i][1]}`;
+    // Create element and set the third element of each book on html
+    const readBook = document.createElement("p");
+    readBook.classList.add("card-content");
+    readBook.textContent = `Status: ${books[i][2]}`;
+    // Append the card div to the div with id cards
+    cardsContainer.appendChild(div);
+    // Append the paragraph's to the each card (div element created)
+    div.appendChild(titleBook);
+    div.appendChild(pagesBook);
+    div.appendChild(readBook);
+  }
+}
+
+// Function to Add Book to Library
 function addBookToLibrary(event) {
   // Prevent the page to reload itself when submit
   event.preventDefault();
@@ -43,10 +82,12 @@ function addBookToLibrary(event) {
   // Create a new book ()
   newBook = new Book(myBook[0], myBook[1], myBook[2], myBook[3]);
   // Store the new Book on library in the format required
+  console.log(newBook);
   myLibrary.push(newBook.info());
   // Clear the array with user input
   myBook = [];
-  console.log(newBook.info());
+  // Display the updated version of the lybrary
+  displayBooks(myLibrary);
 }
 
 // formBook - Store all elements of the form
@@ -58,16 +99,4 @@ const selectBookRead = document.getElementById("read");
 // elements have been inputed by the user  (does not start the event otherwise)
 formBook.addEventListener("submit", addBookToLibrary);
 
-function addBookToLibrary() {}
-
-myLibrary.push(["The Odin List of Treasures by Odin", "220", "read"]);
-myLibrary.push(["The Emperor by Shiklun", "145", "read"]);
-myLibrary.push([
-  "The Girl of the Blue Train by Shiklun",
-  "173",
-  "not read yet",
-]);
-myLibrary.push(["The Shinoby of the Sand by Ninpo", "85", "not read yet"]);
-// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
-
-// console.log(theHobbit.info());
+displayBooks(myLibrary);
